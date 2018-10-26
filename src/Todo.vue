4b40842d40f0cf4e281c936fba0bb3bc6f3ac725
wanglinlin01 <wanglinlin01@zhihu.com>
2018-10-26 16:08:48 +0800

    add todolist

---
+++ src/Todo.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <h3>todo list</h3>
+    <TodoItem v-for="todo in list"
+              :todo="todo"
+              @delete="onDeleteItem"
+              :key="todo.id"
+    />
+    <input type="text" v-model="todo" v-on:keyup.enter="createNewToDoItem"/>
+    <div class="Todo-add" @click="createNewTodoItem"> + </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'todo',
+  data() {
+    return {
+      list: [
+        {
+          'text': 'chean the house',
+        },
+        {
+          'text': 'chean the house',
+        }
+      ],
+      todo: ''
+    }
+  },
+  methods: {
+    createNewTodoItem () {
+      this.list.push({
+        'text': this.todo
+      })
+      this.todo = ''
+    },
+    onDeleteItem(todo) {
+      this.list = this.list.filter(item=> item !== todo)
+    }
+  },
+  components: {
+    TodoItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="TodoItem">
+    <p class="TodoItem-Text">{{todo.text}}</p>
+    <div class="TodoItem-Delete" @click="deleteItem(todo)"> - </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'to-do-item',
+  props: ['todo'],
+  methods: {
+    deleteItem(todo) {
+      this.$emit('delete', todo)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.TodoItem{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.TodoItem-Text {
+  width: 90%;
+  background: #fff;
+  border: solid 1px #ddd;
+  padding: 12px;
+  margin-right: 10px;
+}
+</style>
